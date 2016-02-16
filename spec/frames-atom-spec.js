ebb13b5d8b7ad2308dede47a3845abb4ade3936a
Helder Rossa <kimus.linuxus@gmail.com>
2016-02-16 18:16:35 +0000

    initial import

---
+++ spec/frames-atom-spec.js
@@ -0,0 +1,14 @@
+describe("Frames Atom package", function()
+{
+	describe("just an example", function()
+	{
+		it("some example", function()
+		{
+			runs(function()
+			{
+				var a = 12;
+				expect(a).toEqual(12);
+			});
+		});
+	});
+});
